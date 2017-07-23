2e0e1083f4ed2d19e9c86a8bd67280af1f67e703
LeeRising <leerising773@gmail.com>
2017-07-23 10:43:35 +0300

    Push project to GitHub

---
+++ CreateApiKey/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateApiKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("id:");
+            var id = Console.ReadLine();
+            Console.Write("Name:");
+            var name = Console.ReadLine();
+            var hash = Sha1Hash(DateTime.Now.ToString("dd-MM-yy HH:mm:ss") + id + name);
+            Console.WriteLine(hash);
+        }
+        public static string Sha1Hash(string input)
+        {
+            var hash = new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(input));
+            return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+        }
+    }
+}
